612dd339bf88186728e7d1f30c067aa0f75c17dc
Diego Rodrigues Silva <91738379+ddiego8@users.noreply.github.com>
2021-10-18 09:30:25 -0300

    Create findnumber.py

---
+++ findnumber.py
@@ -0,0 +1,8 @@
+find = False
+print ('Starter condition:', find)
+for x in [8,4,6,1,3,100,20,30,16,49,49,13,3,22,11,22,5,8,7]:
+    if x == 22:
+        find = True
+        break
+        
+print('Final condition:', find)
